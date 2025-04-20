1874a3e7205af4f7d6af2501fb0c0af38d2b1ef7
Agus Supriyadi <ayahnya.fiqa@gmail.com>
2025-04-20 20:04:08 +0700

    Create kenangan-abadi.php

---
+++ kenangan-abadi.php
@@ -0,0 +1,21 @@
+<?php
+include 'header.php';
+?>
+    <main>
+        <section>
+            <h2>Kenangan Abadi: Keluarga yang Sudah Wafat</h2>
+            <p>Mari kita mengenal dan mendoakan keluarga kita yang sudah meninggal.</p>
+            <h3>Daftar Anak</h3>
+            <ol>
+                <li>Mbah Muryadi sekalian (Cilacap)</li>
+				<li>Mbah Sanwarid sekalian (Karang Talun)</li>
+				<li>Mbah Sutarno bin Muryadi</li>
+				<li>Mbah Ngadisem binti Sanwarid</li>
+				<li>Bapak H. Sujamin Ahmad Ansori bin Sutarno</li>
+				<li>Bapak Saebani bin Sutarno</li>
+            </ol>
+        </section>
+    </main>
+<?php
+include 'footer.php';
+?>
