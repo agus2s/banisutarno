0bf0d58551f424ed141a432df64153650601eb42
Agus Supriyadi <ayahnya.fiqa@gmail.com>
2025-04-21 18:52:54 +0700

    Create silsilah.php

---
+++ silsilah.php
@@ -0,0 +1,13 @@
+<?php
+include 'header.php';
+?>
+    <main>
+        <section>
+            <h2>Silsilah Keluarga</h2>
+            <object type="image/svg+xml" data="silsilah.svg"></object>
+
+        </section>
+    </main>
+<?php
+include 'footer.php';
+?>
