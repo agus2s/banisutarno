63a74fbc2f4f29aeeeda1beb3411d54c6375a381
Agus Supriyadi <ayahnya.fiqa@gmail.com>
2025-04-22 10:55:22 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
+const DIAGRAM = 'graph TD
+    A[Mbah Muryadi] --> C[Mbah Sutarno]
+    B[Mbah Sanwarid] --> D[Mbah Ngadisem]
+    C --> Anak-anak
+    D --> Anak-anak
+    ';
 include 'header.php';
 ?>
     <main>
@@ -18,6 +24,10 @@ include 'header.php';
                 <li><a href="keluarga-asep.php">Ibu Hj. Siti Jaenah bersuami Bapak H. Asep Rochaenasyir</a></li>                
                 <li><a href="keluarga-sitinuriyah.php">Ibu Siti Nuriyah</a></li>
             </ol>
+        </section>
+        <section>
+            <h3>Garis Keturunan</h3>
+            <div id="output"></div>
         </section>
     </main>
 <?php
