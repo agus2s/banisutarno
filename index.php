0fac1f51b1f296370df10792c32ad9093a3c7cd2
Agus Supriyadi <ayahnya.fiqa@gmail.com>
2025-04-22 10:57:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -30,6 +30,17 @@ include 'header.php';
             <div id="output"></div>
         </section>
     </main>
+    <script type="module">
+			import mermaid from "https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.esm.min.mjs";
+			mermaid.initialize({ startOnLoad: false });
+			async function renderMermaid() {
+				const elementId = "mermaidChart";
+				const diagramDefinition = <?=DIAGRAM?>;
+				const { svg } = await mermaid.render(elementId, diagramDefinition);
+				document.getElementById("output").innerHTML = svg;
+			}
+			renderMermaid();
+		</script>
 <?php
 include 'footer.php';
 ?>
