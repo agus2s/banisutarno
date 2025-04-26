95895d5b43ec044958a64fa84776063dc278e56a
Agus Supriyadi <ayahnya.fiqa@gmail.com>
2025-04-26 23:05:40 +0700

    Update keluarga-sudarno.php

--- keluarga-sudarno.php
+++ keluarga-sudarno.php
@@ -1,5 +1,9 @@
 <?php include 'header.php'; ?>
     <main>
+        <script type="module">
+            import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs';
+            mermaid.initialize({ startOnLoad: true });
+        </script>
         <section>
             <h2>Bapak H. Sudarno beristri Ibu Sri Suratmi</h2>
             <p>Bapak H. Sudarno adalah anak kelima dari <a href="index.php">Mbah H. Muhammad Sutarno dan Mbah Hj. Ngadisem</a>. Beliau beristri Ibu Sri Suratmi dan memiliki 3 orang anak.</p>
@@ -10,5 +14,14 @@
                 <li>Arif Tri Pamungkas</li>
             </ol>
         </section>
+        <section>
+		<h3>Diagram Keluarga</h4>
+		<div class="mermaid">
+		flowchart TD
+			A["Mbah Sutarno"] --> B["Bapak Sudarno"]
+			click A "index.php"
+			click B "keluarga-sudarno.php"
+		</div>
+	</section>
     </main>
 <?php include 'footer.php'; ?>
