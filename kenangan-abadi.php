22513ad9bbcd7d6791b4cd4c53740a43c54f85bd
Agus Supriyadi <ayahnya.fiqa@gmail.com>
2026-02-19 22:49:51 +0700

    Update kenangan-abadi.php

--- kenangan-abadi.php
+++ kenangan-abadi.php
@@ -4,17 +4,51 @@ include 'header.php';
     <main>
         <section>
             <h2>Kenangan Abadi: Keluarga yang Sudah Wafat</h2>
-            <p>Mari kita mengenal dan mendoakan keluarga kita yang sudah meninggal.</p>
+            <p>Mari kita mengenal dan mendoakan anggota keluarga kita yang sudah meninggal.</p>
             <ol>
-                <li>Mbah Muryadi sekalian (Cilacap)</li>
-				<li>Mbah Sanwarid sekalian (Karang Talun)</li>
-				<li>Mbah Sutarno bin Muryadi</li>
-				<li>Mbah Ngadisem binti Sanwarid</li>
-				<li>Ibu Suyati binti Suparjo</li>
+                <li>Mbah Muryadi sekalian</li>
+				<li>Mbah Sanwarid sekalian</li>
+				<li>Mbah H.M. Soetarno bin Mbah Muryadi sekalian</li>
+				<li>Mbah San Muhidi bin Mbah Sanwikarta sekalian</li>
+				<li>Mbah Sarso Saryono bin Maskilam</li>
+				<li>Mbah Suparjo sekalian</li>
+				<hr>
+				<li>Mbah Mad Winata sekalian</li>
+				<li>Mbah Bunandir bin Sodikin sekalian</li>
+				<li>Mbah Sadiman Partotinoyo sekalian</li>
+				<li>Mbah H. Rohani sekalian</li>
+				<li>Mbah Dulah Ngarsis</li>
+				<li>Mbah Mama Sukarta sekalian</li>
+				<li>Bapak H. Sujamin Ansori bin H.M. Soetarno</li>
+				<hr>
+				<li>Bapak Saebani bin H.M. Soetarno</li>
+				<li>Ibu Suyati binti Mbah Suparjo</li>
 				<li>Ibu Rohmah binti Bunandir</li>
-				<li>Bapak H. Sujamin Ahmad Ansori bin Sutarno</li>
-				<li>Bapak Saebani bin Sutarno</li>
-            </ol>
+				<li>Bapak Untung Subagyo bin Fulan</li>
+				<li>Mbah Singorejo sekalian</li>
+				<li>Mbah Tirtawireja sekalian</li>
+				<hr>
+				<li>Bapak Daliman bin Mbah Singorejo sekalian</li>
+				<li>Nak Pramudita Agam Munggaran bin H. Asep Rokhaenasyr</li>
+				<li>Nak Dwi Apriani binti Saebani</li>
+				<li>Nak Intan Cahya Nugraheni bin Teguh Purwadi Nugroho</li>
+				<li>Nak Tedria Nugroho bin Teguh Purwadi Nugroho</li>
+				<li>Mbah Sandiarja sekalian</li>
+				<hr>
+				<li>Mbah Hadi Sukarto sekalian</li>
+				<li>Mbah Usoy bin Akimun</li>
+				<li>Mbah Basiran Marto Prayitno</li>
+				<li>Mbah Paiman bin Fulan</li>
+				<li>Mbah H. Hasan Ilyas sekalian</li>
+				<li>Mbah Suwardi bin Sutadi</li>
+				<hr>
+				<li>Mbah Dul Kohar</li>
+				<li>Mbah Mungawam bin Sujangi</li>
+				<li>Mbah Surawi bin Madari</li>
+				<li>Bapak Samsidi bin Bapak Sanmuhidi</li>
+				<li>Bapak Iswan bin Bapak Sanmuhidi</li>
+				<li>Ibu Raswen binti Sanmuhidi</li>
+			</ol>
         </section>
     </main>
 <?php
